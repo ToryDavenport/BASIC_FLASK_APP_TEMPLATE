84cf92421ac97ac591b9661425cd241e856ffe32
torydavenport <contact@torydavenport.com>
2020-07-07 02:00:00 +0000

    got a working test!

--- tests/sample-app.py
+++ tests/sample-app.py
@@ -2,14 +2,16 @@ import flask
 from flask import Flask
 from flask import request
 from flask import render_template
+import pytest   
 
 sample = Flask(__name__)
-template = "index.html"
+template = "idex.html"
 
 @sample.route("/")
-def main():
-    assert template == "index.html" 
+def main(): 
     return render_template(template)
-     
-if __name__ == "__main__":
-     sample.run(port=8080, host="10.2.0.100")
+
+def test():
+    assert template == "index.html" 
+if __name__ == "__main__": 
+    sample.run(port=8080, host="10.2.0.100")
